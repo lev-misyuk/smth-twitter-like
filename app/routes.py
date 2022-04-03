e494f6cacc5ea92270d910e3f41cddbb98fe313f
Lev Misyuk <l.misyuk@yandex.ru>
2022-04-03 16:45:16 +0300

    feat: :sparkles: add base and index templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'Lev'}
+    posts = [
+        {
+            'author': {'username': 'Lev'},
+            'body': 'Beautiful day in Moscow!'
+        },
+        {
+            'author': {'username': 'aaa'},
+            'body': 'bbb'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
